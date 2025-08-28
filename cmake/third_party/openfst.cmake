2c1ecf0e7f7fd969a243bb0d500c5ad94ee9d679
asd158 <gaowycn@qq.com>
2025-08-28 16:03:36 +0800

    ++ fst local&& -- cuda

--- cmake/third_party/openfst.cmake
+++ cmake/third_party/openfst.cmake
@@ -1,88 +1,74 @@
-cmake_minimum_required(VERSION 3.14)
-include(FetchContent)
+CMAKE_MINIMUM_REQUIRED(VERSION 3.14)
 
-FetchContent_Declare(
-        openfst
-        GIT_REPOSITORY  https://github.com/kkm000/openfst
-        GIT_TAG         338225416178ac36b8002d70387f5556e44c8d05 # tag win/1.7.2.1
-)
+SET(openfst_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/openfst-src)
+SET(openfst_BINARY_DIR ${CMAKE_BINARY_DIR}/openfst-build)
+INCLUDE_DIRECTORIES(${openfst_SOURCE_DIR}/src/include)
 
-FetchContent_GetProperties(openfst)
-if(NOT openfst_POPULATED)
-    FetchContent_Populate(openfst)
-    include_directories(${openfst_SOURCE_DIR}/src/include)
+ADD_SUBDIRECTORY(${openfst_SOURCE_DIR} ${openfst_BINARY_DIR})
 
-    add_subdirectory(${openfst_SOURCE_DIR} ${openfst_BINARY_DIR})
-
-
-    if(CONDA_ROOT)
-
-        if(MSVC)
-            install(TARGETS fstarcsort fstclosure fstcompile fstcompose fstconcat fstconnect fstconvert fstdeterminize
-                            fstdifference fstdisambiguate fstdraw fstencode fstepsnormalize fstequal fstequivalent
-                            fstinfo fstintersect fstinvert fstisomorphic fstmap fstminimize fstprint fstproject fstprune
-                            fstpush fstrandgen fstrelabel fstreplace fstreverse fstreweight fstrmepsilon fstshortestdistance
-                            fstshortestpath fstsymbols fstsynchronize fsttopsort fstunion
-            RUNTIME
+IF (CONDA_ROOT)
+    IF (MSVC)
+        INSTALL(TARGETS fstarcsort fstclosure fstcompile fstcompose fstconcat fstconnect fstconvert fstdeterminize
+                fstdifference fstdisambiguate fstdraw fstencode fstepsnormalize fstequal fstequivalent
+                fstinfo fstintersect fstinvert fstisomorphic fstmap fstminimize fstprint fstproject fstprune
+                fstpush fstrandgen fstrelabel fstreplace fstreverse fstreweight fstrmepsilon fstshortestdistance
+                fstshortestpath fstsymbols fstsynchronize fsttopsort fstunion
+                RUNTIME
                 DESTINATION ${CMAKE_INSTALL_BINDIR}
                 COMPONENT kaldi
-            )
-            install(TARGETS farcompilestrings farcreate farequal farextract farinfo farisomorphic farprintstrings
-            RUNTIME
+                )
+        INSTALL(TARGETS farcompilestrings farcreate farequal farextract farinfo farisomorphic farprintstrings
+                RUNTIME
                 DESTINATION ${CMAKE_INSTALL_BINDIR}
                 COMPONENT kaldi
-            )
-            install(TARGETS fstlinear fstloglinearapply
-            RUNTIME
+                )
+        INSTALL(TARGETS fstlinear fstloglinearapply
+                RUNTIME
                 DESTINATION ${CMAKE_INSTALL_BINDIR}
                 COMPONENT kaldi
-            )
-            install(TARGETS mpdtcompose mpdtexpand mpdtinfo mpdtreverse
-            RUNTIME
+                )
+        INSTALL(TARGETS mpdtcompose mpdtexpand mpdtinfo mpdtreverse
+                RUNTIME
                 DESTINATION ${CMAKE_INSTALL_BINDIR}
                 COMPONENT kaldi
-            )
-            install(TARGETS pdtcompose pdtexpand pdtinfo pdtreplace pdtreverse pdtshortestpath
-            RUNTIME
+                )
+        INSTALL(TARGETS pdtcompose pdtexpand pdtinfo pdtreplace pdtreverse pdtshortestpath
+                RUNTIME
                 DESTINATION ${CMAKE_INSTALL_BINDIR}
                 COMPONENT kaldi
-            )
-            install(TARGETS fstspecial
-            RUNTIME
+                )
+        INSTALL(TARGETS fstspecial
+                RUNTIME
                 DESTINATION ${CMAKE_INSTALL_BINDIR}
                 COMPONENT kaldi
-            )
+                )
 
-            install(DIRECTORY ${openfst_SOURCE_DIR}/src/include/ DESTINATION include/
-                    COMPONENT kaldi
-                    FILES_MATCHING PATTERN "*.h")
-
-            install(TARGETS fst
-                    EXPORT kaldi-targets
-                    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT kaldi
-                    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT kaldi NAMELINK_SKIP
-                    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT kaldi)
-        else()
-
-            install(TARGETS fst
-                    LIBRARY
-                        DESTINATION ${CMAKE_INSTALL_LIBDIR}
-                        COMPONENT kaldi
-                        NAMELINK_SKIP # Link to so with version to avoid conflicts with OpenFst 1.8.1 on conda
-                    )
-
-        endif()
-
-    else() # Original functionality
-
-        install(DIRECTORY ${openfst_SOURCE_DIR}/src/include/ DESTINATION include/
+        INSTALL(DIRECTORY ${openfst_SOURCE_DIR}/src/include/ DESTINATION include/
+                COMPONENT kaldi
                 FILES_MATCHING PATTERN "*.h")
 
-        install(TARGETS fst
+        INSTALL(TARGETS fst
                 EXPORT kaldi-targets
-                ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-                LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-                RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+                ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT kaldi
+                LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} COMPONENT kaldi NAMELINK_SKIP
+                RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT kaldi)
+    ELSE ()
 
-    endif()
-endif()
+        INSTALL(TARGETS fst
+                LIBRARY
+                DESTINATION ${CMAKE_INSTALL_LIBDIR}
+                COMPONENT kaldi
+                NAMELINK_SKIP # Link to so with version to avoid conflicts with OpenFst 1.8.1 on conda
+                )
+
+    ENDIF ()
+
+ELSE () # Original functionality
+    INSTALL(DIRECTORY ${openfst_SOURCE_DIR}/src/include/ DESTINATION include/
+            FILES_MATCHING PATTERN "*.h")
+    INSTALL(TARGETS fst
+            EXPORT kaldi-targets
+            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+ENDIF ()
